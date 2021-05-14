e73567e3324495a177bc1bb0d5864a877c4d1201
alextran0899 <alextran@MacBook-Air.local>
2021-05-13 23:37:36 -0500

    double left join working

--- api/model.js
+++ api/model.js
@@ -3,8 +3,9 @@ const db = require("../data/db-config")
 function getRecipeById(id) {
     console.log('inside module', id)
     return db('recipes as r')
-    .select("r.*", )
+    .select("r.*", "st.*", "i.*")
     .leftJoin('steps as st', 'r.recipe_id', 'st.recipe_id')
+    .leftJoin('ingredients as i', 'st.steps_id', 'i.steps_id')
     .where('r.recipe_id', id)
 }
 
