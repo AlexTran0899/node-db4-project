91eae45db70cab96dfc2e82591079f32cfbb21c6
alextran0899 <alextran@MacBook-Air.local>
2021-05-13 22:49:14 -0500

    made database

--- data/migrations/20210514031712_recipe.js
+++ data/migrations/20210514031712_recipe.js
@@ -1,8 +1,41 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('recipes', tbl => {
+        tbl.increments('recipe_id')
+        tbl.string('recipe_name', 255).notNullable()
+        tbl.date('created_at').notNullable().defaultTo(knex.fn.now())
+    })
+    .createTable('steps', tbl => {
+        tbl.increments('steps_id')
+        tbl.string("step_instruction")
+        tbl.integer("step_number")
+        tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references('scheme_id')
+        .inTable('schemes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments('ingredients_id')
+        tbl.string("ingredient_name")
+        tbl.string("quanity")
+        tbl.integer("steps_id")
+        .unsigned()
+        .notNullable()
+        .references("steps_id")
+        .inTable("steps")
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists("recipes")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("ingredients")
 };
